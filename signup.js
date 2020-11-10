41c6e89578cc2792920f7ceaf67b2e694d724aae
zainab <zaynabfalah7@gmail.com>
2020-11-10 22:05:31 +0300

    sweet commit

---
+++ signup.js
@@ -0,0 +1,13 @@
+
+ 
+//زينب فلاح محمد/معلومات /صباحي
+
+
+
+
+
+function myFunction() {
+    var x = document.getElementById("msg");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
